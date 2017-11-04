22d18ad312e9907ae497c93b86a462a6428ccddc
Sam Neaves <sam.neaves@gmail.com>
2017-11-04 06:56:23 +0000

    Added basic webpage.

--- basic_site.pl
+++ basic_site.pl
@@ -11,8 +11,8 @@
 
 %:- initialization http_daemon.
 
-:- http_handler(root(alexa), alexa,[]).
-
+:- http_handler(root(alexa), alexa,[])
+:- http_handler('/', say_hi, []).
 % The predicate server(+Port) starts the server. It simply creates a
 % number of Prolog threads and then returns to the toplevel, so you can
 % (re-)load code, debug, etc.
@@ -21,3 +21,23 @@ server(Port) :-
 
 
 
+say_hi(_Request) :-
+	phrase(
+	    my_nonterm,
+	    TokenizedHtml,
+	    []),
+        format('Content-type: text/html~n~n'),
+	print_html(TokenizedHtml).
+
+my_nonterm -->
+	html([html([head([title('Sam Neaves homepageHeroku Test')]),
+		    body([h1('Heroku Test'),
+			  p("Please take a look at these:"),
+                          h2('Youtube Channel Playing with Prolog:'),
+			  a(href='https://www.youtube.com/channel/UCfWpIHmy5MEx2p9c_GJrE_g', 'Youtube Playing with Prolog'),
+			  h2('Remote controlling a robot with Prolog pengines:'),
+                          
+			  p('This server is powered by swi-prolog and Heroku')
+                        ])
+                   ])
+             ]).
